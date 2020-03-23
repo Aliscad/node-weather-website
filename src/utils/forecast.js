0e8d44d3c68fbd2f00ed3bc770ac1143dfb7c925
Wei Yan <wei.yan@ibm.com>
2020-03-22 18:14:13 -0700

    Add forecast for tomorrow

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const forecast = (latitiude, longitude, callback) => {
             callback('Unable to find location', undefined)
         }else {
             callback(undefined ,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature 
-                + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+                + ' degrees out with . There is a ' + body.currently.precipProbability 
+                + '% chance of rain. Tomorrow will be ' + body.daily.data[1].summary)
         }
     })
 }
